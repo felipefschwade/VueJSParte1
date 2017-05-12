3e5a9cc4acf970bb0826602bab9bdbdd38420868
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-05-11 23:34:10 -0400

    Criando uma nova propriedade para utilizar textos em rotas. Utilizando as rotas para criar um menu de navegação.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="corpo">
+    <nav>
+      <ul>
+        <li v-for="rota of routes">
+          <router-link :to="rota.path ? rota.path : '/'">{{ rota.text }}</router-link>
+        </li>
+      </ul>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
 <script>
-
+  import { routes } from './routes';
+  export default {
+      data() {
+        return {
+          routes
+        }
+      }
+  }
 </script>
 <style scoped>
   .corpo {
